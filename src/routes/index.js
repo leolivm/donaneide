d2ddab882369f7c07e5974018e5f4a88693db596
leolivm <leolivm@outlook.com>
2021-12-16 23:38:01 -0300

    change matchd example id

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,7 @@ const routes = Router()
 
 routes.use('/', async (req, res) => {
   const { data } = await axios.get(
-    `https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/v001/?key=${config.STEAM_KEY}&match_id=6320553119`
+    `https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/v001/?key=${config.STEAM_KEY}&match_id=6320609816`
   )
 
   return res.json({ data })
