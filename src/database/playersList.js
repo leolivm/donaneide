8fa3968adb6081b258fb1a4eed145bccb7e84fcc
leolivm <leolivm@outlook.com>
2021-12-17 20:13:48 -0300

    adding player ids

--- src/database/playersList.js
+++ src/database/playersList.js
@@ -1,10 +1,54 @@
 export default [
   {
-    account_id: '124304761',
-    steam_id: '76561198084570489',
+    account_id: '158991411',
+    steam_id: '76561198119257139',
+  },
+  {
+    account_id: '124320458',
+    steam_id: '76561198084586186',
   },
   {
     account_id: '149115185',
     steam_id: '76561198109380913',
+  },
+  {
+    account_id: '113902617',
+    steam_id: '76561199099568345',
+  },
+  {
+    account_id: '91346073',
+    steam_id: 'furyking',
+  },
+  {
+    account_id: '184644917',
+    steam_id: '76561198144910645',
+  },
+  {
+    account_id: '145083814',
+    steam_id: '76561198105349542',
+  },
+  {
+    account_id: '222454852',
+    steam_id: '76561198182720581',
+  },
+  {
+    account_id: '149451920',
+    steam_id: 'grillomafioso',
+  },
+  {
+    account_id: '123490103',
+    steam_id: '76561198083755831',
+  },
+  {
+    account_id: '115046394',
+    steam_id: 'tatinha25',
+  },
+  {
+    account_id: '133770475',
+    steam_id: '76561198094036203',
+  },
+  {
+    account_id: '210881971',
+    steam_id: '76561198171147699',
   },
 ]
