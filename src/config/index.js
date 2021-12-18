f6db5221c856f391a397e6547f7a15beb3a2b6c9
leolivm <leolivm@outlook.com>
2021-12-18 02:15:55 -0300

    add testing id channel

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
-  CHANNEL: process.env.CHANNEL_ID,
+  CHANNEL: process.env.TESTING_ID,
   TOKEN: process.env.DISCORD_TOKEN,
   STEAM_KEY: process.env.STEAM_WEB_API,
   time: 86400000,
